aad117938e3469bd6b30c6da5311b368cfe7e6a8
luisfpeluyera <luis.f.peluyera@gmail.com>
2023-12-04 16:10:14 -0600

    street eatz project init

---
+++ src/main/java/com/example/streeteatz/StreetEatzApplication.java
@@ -0,0 +1,13 @@
+package com.example.streeteatz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetEatzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreetEatzApplication.class, args);
+    }
+
+}
