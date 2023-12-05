40661ac3e2bd05b9d7b57236825f4a568da54bb1
Gerald Walker <gerald.e.walker05@gmail.com>
2023-12-05 15:13:58 -0600

    db fixed

--- src/g's-tester/mysql/seeder/reviews_seeder.sql
+++ src/g's-tester/mysql/seeder/reviews_seeder.sql
@@ -1,3 +1,3 @@
 use capstone_db;
 
-INSERT INTO capstone_db.reviews (user_name, truck_name, rating, comment, pictures) VALUES (1, 1, 10, 'Amazing Truck', '111222333')
+INSERT INTO capstone_db.reviews (user_id, truck_id, rating, comment, pictures) VALUES (1, 1, 10, 'Amazing Truck', '111222333')
