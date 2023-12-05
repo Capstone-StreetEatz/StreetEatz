f6213a77247288f32bacec2141bcdf805e5befc3
Gerald Walker <gerald.e.walker05@gmail.com>
2023-12-05 15:06:48 -0600

    db good

--- src/g's-tester/mysql/migration/trucks.sql
+++ src/g's-tester/mysql/migration/trucks.sql
@@ -6,11 +6,11 @@ CREATE TABLE trucks
 (
     id            INT UNSIGNED NOT NULL AUTO_INCREMENT,
     truck_name    VARCHAR(255) NOT NULL,
-    owners_name   INT UNSIGNED NOT NULL,
+    owners_id   INT UNSIGNED NOT NULL,
     location      VARCHAR(1000) NOT NULL,
     pictures      VARCHAR(2000) Not NULL,
 
     PRIMARY KEY (id),
-    FOREIGN KEY (owners_name) references users (id)
+    FOREIGN KEY (owners_id) references users (id)
 );
 
