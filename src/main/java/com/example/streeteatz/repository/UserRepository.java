75e0af0b743f28fb204cf2290545ec376ce34614
luisfpeluyera <luis.f.peluyera@gmail.com>
2023-12-07 16:03:26 -0600

    edit review logic added

--- src/main/java/com/example/streeteatz/repository/UserRepository.java
+++ src/main/java/com/example/streeteatz/repository/UserRepository.java
@@ -1,12 +1,18 @@
 package com.example.streeteatz.repository;
 
 
+import com.example.streeteatz.model.Review;
 import com.example.streeteatz.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     User getUserById(int id);
     User findByUsername(String username);
+
+
+
 
 }
