8bb7474ffc3aab1350435c381c6c0f7a2bf6861c
Milianigia Rios <nanirios671@gmail.com>
2023-12-11 11:03:51 -0600

    fixed

--- src/main/java/com/example/streeteatz/controller/ShowAllController.java
+++ src/main/java/com/example/streeteatz/controller/ShowAllController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ShowAllController {
 
-    @GetMapping("/showall")
+    @GetMapping("/showAll")
 
     public String hello() {
 
--- src/main/java/com/example/streeteatz/services/SecurityConfiguration.java
+++ src/main/java/com/example/streeteatz/services/SecurityConfiguration.java
@@ -43,13 +43,7 @@ public class SecurityConfiguration {
 
                         /* Pages that do not require authentication
                          * anyone can visit the home page, register, login, and view ads */
-                        .requestMatchers("/", "/reviews", "/reviews/*", "/sign-up", "/login", "/about", "/index", "/showall").permitAll()
-
-
-                        /* Pages that do not require authentication
-                         * anyone can visit the home page, register, login, and view ads */
-                        .requestMatchers("/", "/reviews", "/reviews/**", "reviews/show", "reviews/{id}", "/sign-up", "/login", "/about", "/index", "showall").permitAll()
-                        // allow loading of static resources
+                        .requestMatchers("/", "/reviews", "/reviews/*", "/sign-up", "/login", "/about", "/index", "/showAll").permitAll()
 
 
                         .requestMatchers("/css/**", "/js/**", "/img/**","/partials/**", "/partials_css/**", "/assets/**").permitAll()
